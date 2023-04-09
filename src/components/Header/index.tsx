4686ad2530c6950ceb12a0148a7a6c18f2efd74b
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-04-09 19:51:02 -0300

    feat: implement New Transaction Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 // COMPONENT
 import { Button } from '../Button'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 // STYLE
 import { headerContainer, headerContent, logoContainer } from './styles.css'
 
 // ASSET
 import logo from '../../assets/logo.svg'
+import { Plus } from 'phosphor-react'
 
 export function Header() {
   return (
@@ -16,7 +19,16 @@ export function Header() {
           <p>Nobills</p>
         </div>
 
-        <Button>New transaction</Button>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <Button>
+              New transaction
+              <Plus />
+            </Button>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </div>
     </header>
   )
