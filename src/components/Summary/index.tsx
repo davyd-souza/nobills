baea5d74672031622c4d35cc9474760bc9caddc8
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-04-13 01:38:28 -0300

    feat: implement summary dynamically

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,3 +1,9 @@
+// DEPENDENCY
+import { useContext } from 'react'
+
+// CONTEXT
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+
 // STYLE
 import { vars } from '../../styles/global.css'
 import {
@@ -14,6 +20,27 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 type SummaryArticleProps = SummaryCardVariants & {}
 
 export function Summary({ variant = 'gray' }: SummaryArticleProps) {
+  const { transactions } = useContext(TransactionsContext)
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+
   return (
     <section className={SummaryWrapper}>
       <article className={SummaryCard({ variant })}>
@@ -21,7 +48,9 @@ export function Summary({ variant = 'gray' }: SummaryArticleProps) {
           <span className={SummaryTitle({ variant })}>Income</span>
           <ArrowCircleUp size={32} color={vars.color.green[500]} />
         </header>
-        <strong className={SummaryAmount}>$ 17,400.00</strong>
+        <strong className={SummaryAmount}>
+          $ {summary.income.toLocaleString()}
+        </strong>
       </article>
 
       <article className={SummaryCard({ variant })}>
@@ -29,7 +58,9 @@ export function Summary({ variant = 'gray' }: SummaryArticleProps) {
           <span className={SummaryTitle({ variant })}>Outcome</span>
           <ArrowCircleDown size={32} color={vars.color.red[500]} />
         </header>
-        <strong className={SummaryAmount}>$ 1,259.00</strong>
+        <strong className={SummaryAmount}>
+          $ {summary.outcome.toLocaleString()}
+        </strong>
       </article>
 
       <article className={SummaryCard({ variant: 'yellow' })}>
@@ -37,7 +68,9 @@ export function Summary({ variant = 'gray' }: SummaryArticleProps) {
           <span className={SummaryTitle({ variant: 'yellow' })}>Total</span>
           <CurrencyDollar size={32} color={vars.color.gray[800]} />
         </header>
-        <strong className={SummaryAmount}>$ 16,141.00</strong>
+        <strong className={SummaryAmount}>
+          $ {summary.total.toLocaleString()}
+        </strong>
       </article>
     </section>
   )
