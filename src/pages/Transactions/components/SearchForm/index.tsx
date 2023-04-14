f5debb53df4501320cd63906b7d701b040f9a047
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-04-14 00:14:16 -0300

    refactor: implement functionality to search form

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,17 +1,54 @@
+// DEPENDENCY
+import { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+// CONTEXT
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
+
 // STYLE
 import { MagnifyingGlass } from 'phosphor-react'
 import { button } from '../../../../components/Button/style.css'
 import { SearchFormInput, SearchFormWrapper } from './styles.css'
 
+// TYPE
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  const handleSearchTransactions = async ({ query }: SearchFormInputs) => {
+    await fetchTransactions(query)
+  }
+
   return (
-    <form className={SearchFormWrapper}>
+    <form
+      className={SearchFormWrapper}
+      onSubmit={handleSubmit(handleSearchTransactions)}
+    >
       <input
         type="text"
         className={SearchFormInput}
         placeholder="Search for transactions..."
+        {...register('query')}
       />
-      <button className={button({ variant: 'outline' })}>
+      <button
+        className={button({ variant: 'outline' })}
+        disabled={isSubmitting}
+      >
         <MagnifyingGlass size={20} />
         Search
       </button>
