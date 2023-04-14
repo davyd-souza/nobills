7748f5eb4dc5d31775bfaa85b7fc83b980449f62
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-04-13 22:19:29 -0300

    refactor: create hook for summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,5 @@
-// DEPENDENCY
-import { useContext } from 'react'
-
-// CONTEXT
-import { TransactionsContext } from '../../contexts/TransactionsContext'
+// HOOK
+import { useSummary } from '../../hooks/useSummary'
 
 // STYLE
 import { vars } from '../../styles/global.css'
@@ -23,26 +20,7 @@ import { priceFormatter } from '../../utils/formatter'
 type SummaryArticleProps = SummaryCardVariants & {}
 
 export function Summary({ variant = 'gray' }: SummaryArticleProps) {
-  const { transactions } = useContext(TransactionsContext)
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useSummary()
 
   return (
     <section className={SummaryWrapper}>
---
+++ src/hooks/useSummary.tsx
@@ -0,0 +1,30 @@
+// DEPENDENCY
+import { useContext } from 'react'
+
+// CONTEXT
+import { TransactionsContext } from '../contexts/TransactionsContext'
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionsContext)
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+
+  return summary
+}
