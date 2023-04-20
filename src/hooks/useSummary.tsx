e26bfc4e846e26f80177828e29d1216682127e2f
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-04-20 01:00:32 -0300

    refactor: implement useMemo to summary calculation

--- src/hooks/useSummary.tsx
+++ src/hooks/useSummary.tsx
@@ -1,4 +1,5 @@
 // DEPENDENCY
+import { useMemo } from 'react'
 import { useContextSelector } from 'use-context-selector'
 
 // CONTEXT
@@ -10,24 +11,26 @@ export function useSummary() {
     (context) => context.transactions,
   )
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
